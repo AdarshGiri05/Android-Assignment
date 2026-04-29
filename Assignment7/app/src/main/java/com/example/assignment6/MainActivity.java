fefc619e2f91654184dbd817d93ce84c073db7d0
AdarshGiri05 <adarshgiriag2006@gmail.com>
2026-04-29 20:33:58 +0530

    Final update Assignment 6 and 7

--- Assignment7/app/src/main/java/com/example/assignment6/MainActivity.java
+++ Assignment7/app/src/main/java/com/example/assignment6/MainActivity.java
@@ -29,13 +29,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        
         gridView = findViewById(R.id.gridview);
         btn = findViewById(R.id.selectImage);
-        
-        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
